9d2612a19da96b6e941e3992e8ee04b85cf691e1
senzoo-oh <osj3418@gmail.com>
2025-01-05 03:13:07 +0900

    feat: ✨ 앱 네비게이터 구현

---
+++ src/navigation/AppNavigator.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { useAuth } from '../context/AuthContext';
+
+
+import LoginScreen from '../screens/LoginScreen';
+import HomeScreen from '../screens/HomeScreen';
+import SignUpScreen from '../screens/SignUpScreen';
+import GoalSettingScreen from '../screens/GoalSettingScreen';
+import MyPageScreen from '../screens/MyPageScreen';
+
+const Stack = createStackNavigator();
+
+const AppNavigator = () => {
+    const { isLoggedIn } = useAuth();
+
+    return (
+        <NavigationContainer>
+            <Stack.Navigator screenOptions = {{ headerShown: false }}>
+                {
+                isLoggedIn ? (
+                    <Stack.Screen name="Home" component={HomeScreen} />
+                ) : (
+                    <Stack.Screen name="Login" component={LoginScreen} />
+                )
+                }
+                <Stack.Screen name="SignUp" component={SignUpScreen} />
+                <Stack.Screen name="SetGoal" component={GoalSettingScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+};
+
+export default AppNavigator;
